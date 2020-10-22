7eb005790aab3ad3968aee81dbcb386b0915bd0c
1005hoon <shoh@help-me.kr>
2020-10-22 17:45:05 +0900

    2번문제. 모의고사

--- 02-모의고사.js
+++ 02-모의고사.js
@@ -1,19 +1,3 @@
-// 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
-
-// 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-// 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-// 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
-
-// 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
-
-// 제한 조건
-// 시험은 최대 10,000 문제로 구성되어있습니다.
-// 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
-// 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
-// 입출력 예
-// answers	return
-// [1,2,3,4,5]	[1]
-// [1,3,2,4,2]	[1,2,3]
 
 const solution = (numArr) => {
   // 패턴 설정
@@ -22,10 +6,10 @@ const solution = (numArr) => {
   const thirdPattern = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
   // 필터 활용해서 각각 점수 체점
+  // 지금 작업중인 인덱스의 숫자가, 수포자의 패턴에 인덱스에 위치한 숫자랑 같으면 filter 활용해 array로 리턴
+  // 만약 문제의 수가 수포자의 패턴보다 길다면, 지금 작업중인 문제의 index를 수포자 패턴의 길이로 나눈 나머지 값을
+  // 현재 인덱스로 가져와서 계속 파악
   const firstScore = numArr.filter((num, index) => (
-    // 지금 작업중인 인덱스의 숫자가, 수포자의 패턴에 인덱스에 위치한 숫자랑 같으면 filter 활용해 array로 리턴
-    // 만약 문제의 수가 수포자의 패턴보다 길다면, 지금 작업중인 문제의 index를 수포자 패턴의 길이로 나눈 나머지 값을
-    // 현재 인덱스로 가져와서 계속 파악
     num === firstPattern[index % firstPattern.length]
   )).length
   const secondScore = numArr.filter((num, index) => (
@@ -34,7 +18,7 @@ const solution = (numArr) => {
   const thirdScore = numArr.filter((num, index) => (
     num === thirdPattern[index % thirdPattern.length]
   )).length
-  // filter가 ㄷ ㅏ끝나면, 맞춘 답들을 담은 array가 반환될텐데, 거기에 length를 사용해서 
+  // filter가 다끝나면, 맞춘 답들을 담은 array가 반환될텐데, 거기에 length를 사용해서 
   // 점수처럼 가져오기
 
 
