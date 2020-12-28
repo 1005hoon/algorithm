b0fce5dc09edb56cafacdf677fd58f27605e7ef9
Hoon Oh <shoh@help-me.kr>
2020-12-29 06:37:33 +0900

    searching algorithms

---
+++ javascript/02_searching-algorithms/linearSearch.js
@@ -0,0 +1,8 @@
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
+};
+
+console.log(linearSearch([10, 15, 20, 25, 30], 15));
