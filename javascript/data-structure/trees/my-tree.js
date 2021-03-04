dcea91942dfa69d1c9d8a54f6d3fc5af731ccb2f
Hoon <shoh@help-me.kr>
2021-03-04 18:45:24 +0900

    ADD: BST insert feature

--- javascript/data-structure/trees/my-tree.js
+++ javascript/data-structure/trees/my-tree.js
@@ -10,9 +10,40 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
 
   lookup(value) {}
 
   remove(value) {}
 }
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+
+const tree = new BinarySearchTree();
